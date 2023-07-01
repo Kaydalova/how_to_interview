0ec25818efa101ec4a8e5df1acc271b98defdf64
Александра Кайдалова <alexandrakaydalova@yandex.ru>
2023-07-01 17:29:08 +0300

    topic uploading added

--- interview_app/cli_commands.py
+++ interview_app/cli_commands.py
@@ -9,10 +9,11 @@ from .models import Topic, Question
 @app.cli.command('load_topics')
 def load_topics_command():
     """Функция загрузки топиков в базу данных."""
-    with open('topics.csv', encoding='utf-8') as f:
+    with open('topics.csv', encoding='utf-8-sig') as f:
         reader = csv.DictReader(f)
         counter = 0
         for row in reader:
+            print(row)
             opinion = Topic(**row)
             db.session.add(opinion)
             db.session.commit()
@@ -23,7 +24,7 @@ def load_topics_command():
 @app.cli.command('load_questions')
 def load_questions_command():
     """Функция загрузки вопросов в базу данных."""
-    with open('questions.csv', encoding='utf-8') as f:
+    with open('questions.csv', encoding='utf-8-sig') as f:
         reader = csv.DictReader(f)
         counter = 0
         for row in reader:
